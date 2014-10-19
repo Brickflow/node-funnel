a75247a412dff720d5168c2dc42cb1200f6ff34d
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-10-19 19:00:12 +0200

    feat(event-time): calculates funnels based on event times

--- index.js
+++ index.js
@@ -1,3 +1,82 @@
 'use strict';
 
-module.exports = function() {};
+var _ = require('lodash');
+
+function createFunnelCalculator(options) {
+  options = options || {};
+  validateOptions(options);
+
+  return function(funnelDescription) {
+    funnelDescription = funnelDescription || [];
+    var steps = getFunnelSteps(funnelDescription);
+    return _.extend(
+      {steps: steps},
+      getAllStep(steps)
+    );
+  };
+}
+
+function getAllStep(steps) { 
+  function getAllPercent() {
+    if (steps.length === 0) {
+      return 0;
+    }
+    return steps.reduce(function(acc, current) {
+      return acc * current.percent / 100;
+    }, 100);
+  }
+
+  return {
+    all: { 
+      percent: getAllPercent()
+    }
+  };
+}
+
+function getFunnelSteps(funnelDescription) {
+  var minTimes = [];
+
+  function getPercent(index) {
+    if (index === 0) {
+      return 100;
+    }
+    return Object.keys(minTimes[index]).length /
+      Object.keys(minTimes[index - 1]).length * 100;
+  }
+
+  function reduceMinTime(stepIndex) {
+    function isFirstStep() {
+      return stepIndex === 0;
+    }
+
+    function isEventTimeLaterThanLastStep(event) {
+      return event.time >= minTimes[stepIndex - 1][event.id];
+    } 
+
+    return function(acc, event) {
+      if (isFirstStep() || isEventTimeLaterThanLastStep(event)) {
+        var oldTime = acc[event.id] !== undefined ? acc[event.id] : Infinity;
+        acc[event.id] = Math.min(event.time, oldTime);
+      }
+      return acc;
+    };
+  }
+
+  return funnelDescription.map(function(step, index) {
+    minTimes.push(step.events.reduce(reduceMinTime(index), {}));
+    return {
+      name: step.name,
+      count: Object.keys(minTimes[index]).length,
+      percent: getPercent(index)
+    };
+  });
+}
+
+function validateOptions(options) {  
+  if (options.distinctId === undefined) {
+    throw new Error('No distinctId is provided!');  
+  }
+}
+
+module.exports = createFunnelCalculator;
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,7 +3,118 @@
 var test = require('tape');
 var funnel = require('../');
 
-test('funnel-metrics', function(t) {
+test('funnel', function(t) {
   t.equal(typeof funnel, 'function', 'it should exists');
+
+  t.throws(function() {
+    funnel(); 
+  } , /No distinctId/, 'should fail if no distinctId is provided!');
+
+  t.equal(typeof funnel({distinctId: 'id'}), 'function',
+    'it should create the calculator');
+
+  t.end();
+});
+
+function calcFunnel(funnelDescription) {
+  return funnel({distinctId: 'id'})(funnelDescription);
+}
+
+test('calculate funnel empty imputs', function(t) {
+  var emptyDeatils = {
+    steps: [],
+    all: {percent: 0}
+  };
+
+  t.deepEqual(calcFunnel(), emptyDeatils);
+
+  t.deepEqual(calcFunnel([]), emptyDeatils);
+
+  t.end();
+});
+
+test('calculate funnel', function(t) {
+  function assertFunnel(funnelDetails, description) {
+    t.deepEqual(funnelDetails, genFunnelDetails(description));
+  }
+
+  function genFunnelDetails(dummy) {
+    return {
+      steps: dummy.steps.map(function(event, index) {
+        return {
+          name: 'event-' + (index + 1),
+          count: event.c,
+          percent: event.p
+        };
+      }),
+      all: {
+        percent: dummy.all
+      }
+    };
+  }
+
+  function genEvents(eventDetails) {
+    return eventDetails.map(function(event, index) {
+      return {
+        name: 'event-' + (index + 1),
+        events: event
+      };
+    });
+  }
+  
+  (function() {
+    var events = genEvents([
+      [{id: 1, time: 1}]
+    ]);
+    assertFunnel(calcFunnel(events), {
+      steps: [{c: 1, p: 100}],
+      all: 100
+    });
+  }());
+
+  (function() {
+    var events = genEvents([
+      [{id: 1, time: 1}],
+      [{id: 1, time: 1}]
+    ]);
+    assertFunnel(calcFunnel(events), {
+      steps: [{c: 1, p: 100}, {c:1, p: 100}],
+      all: 100
+    });
+  }());
+
+  (function() {
+    var events = genEvents([
+      [{id: 1, time: 1}, {id: 2, time: 1}],
+      [{id: 1, time: 1}]
+    ]);
+    assertFunnel(calcFunnel(events), {
+      steps: [{c: 2, p: 100}, {c: 1, p: 50}],
+      all: 50
+    });
+  }());
+
+  (function() {
+    var events = genEvents([
+      [{id: 1, time: 2}],
+      [{id: 1, time: 1}]
+    ]);
+    assertFunnel(calcFunnel(events), {
+      steps: [{c: 1, p: 100}, {c: 0, p: 0}],
+      all: 0
+    });
+  }());
+
+  (function() {
+    var events = genEvents([
+      [{id: 1, time: 2}, {id: 1, time: 1}],
+      [{id: 1, time: 1}]
+    ]);
+    assertFunnel(calcFunnel(events), {
+      steps: [{c: 1, p: 100}, {c: 1, p: 100}],
+      all: 100
+    });
+  }());
+
   t.end();
 });
